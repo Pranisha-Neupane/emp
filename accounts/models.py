c4e13b013ade5097a090d4aa192dd707439cf1fd
Pranisha-Neupane <npranisha33@gmail.com>
2025-07-02 13:25:54 +0545

    Add all project files

---
+++ accounts/models.py
@@ -0,0 +1,28 @@
+
+from employee.models import Employee
+from datetime import date
+
+# Employee.objects.create(
+#     eID="emp001",
+#     firstName="Pranisha",
+#     middleName="M.",
+#     lastName="Neupane",
+#     phoneNo="9800000000",
+#     email="emp001@example.com",
+#     addharNo="1234-5678-9012",
+#     dOB=date(2002, 1, 1),
+#     designation="Junior Developer",
+#     salary="50000",
+#     joinDate=date(2024, 9, 1)
+
+
+
+# from employee.models import Employee
+# Employee.objects.all()
+
+
+# exit()
+
+
+# <Employee: emp001 Pranisha>
+# )
